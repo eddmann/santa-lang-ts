391dc09727598299e08dcbabd181d3963f034842
Edd Mann <the@eddmann.com>
2022-07-21 20:29:46 +0100

    Add initial CLI AoC template runner

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,10 +1,32 @@
 #!/usr/bin/env node
 
-import { readFileSync } from 'fs';
+import { readFileSync, realpathSync } from 'fs';
 import { Lexer } from '../lexer';
 import { AST, Parser } from '../parser';
 import { evaluate, O } from '../evaluator';
 import printSourcePreview from './printSourcePreview';
+
+const evaluateSection = (
+  section: O.Section,
+  environment: O.Enviornment,
+  input: O.Obj | null = null
+): O.Obj => {
+  const sectionEnvironment = new O.Environment(environment);
+
+  if (input) {
+    sectionEnvironment.declareVariable('input', input, false);
+  }
+
+  const result = evaluate(section.section, sectionEnvironment);
+
+  if (result instanceof O.Err) {
+    printSourcePreview(filename, source, result.node.source.line, result.node.source.column);
+    console.log('\x1b[32m%s\x1b[0m', result.inspect());
+    process.exit(1);
+  }
+
+  return result;
+};
 
 let filename: string, source: string;
 try {
@@ -15,6 +37,9 @@ try {
   console.log('Unable to open source file');
   process.exit(1);
 }
+
+// ensure all file reads are relative to the `.santa` source location
+process.chdir(require('path').dirname(realpathSync(filename)));
 
 const lexer = new Lexer(source);
 if (process.argv.includes('-l')) {
@@ -37,13 +62,159 @@ try {
 }
 
 const environment = new O.Environment();
-const result = evaluate(ast, environment);
 
+const result = evaluate(ast, environment);
 if (result instanceof O.Err) {
   printSourcePreview(filename, source, result.node.source.line, result.node.source.column);
   console.log('\x1b[32m%s\x1b[0m', result.inspect());
   process.exit(1);
 }
 
-console.log(result.inspect());
-process.exit(0);
+const partOne = environment.getSection('part_one');
+const partTwo = environment.getSection('part_two');
+
+if (partOne.length === 0 && partTwo.length === 0) {
+  console.log(result.inspect());
+  process.exit(0);
+}
+
+if (partOne.length > 1) {
+  printSourcePreview(filename, source, partOne[1].name.source.line, partOne[1].name.source.column);
+  console.log('\x1b[32m%s\x1b[0m', 'Runtime error: Expected a single `part_one` section');
+  process.exit(1);
+}
+
+if (partTwo.length > 1) {
+  printSourcePreview(filename, source, partTwo[1].name.source.line, partTwo[1].name.source.column);
+  console.log('\x1b[32m%s\x1b[0m', 'Runtime error: Expected a single `part_two` section');
+  process.exit(1);
+}
+
+const input = environment.getSection('input');
+if (input.length > 1) {
+  printSourcePreview(filename, source, input[1].name.source.line, input[1].name.source.column);
+  console.log('\x1b[32m%s\x1b[0m', 'Runtime error: Expected a single `input` section');
+  process.exit(1);
+}
+
+const inputResult = evaluate(input[0].section, environment);
+if (inputResult instanceof O.Err) {
+  printSourcePreview(
+    filename,
+    source,
+    inputResult.node.source.line,
+    inputResult.node.source.column
+  );
+  console.log('\x1b[32m%s\x1b[0m', inputResult.inspect());
+  process.exit(1);
+}
+
+if (!process.argv.includes('-t')) {
+  const partOneResult = evaluateSection(partOne[0], environment, inputResult);
+  console.log('Part 1: \x1b[32m%s\x1b[0m', partOneResult.inspect());
+
+  const partTwoResult = evaluateSection(partTwo[0], environment, inputResult);
+  console.log('Part 2: \x1b[32m%s\x1b[0m', partTwoResult.inspect());
+
+  process.exit(0);
+}
+
+let exitCode = 0;
+environment.getSection('test').forEach((test, idx) => {
+  const testEnvironment = new O.Environment(environment);
+
+  const testResult = evaluate(test.section, testEnvironment);
+  if (testResult instanceof O.Err) {
+    printSourcePreview(
+      filename,
+      source,
+      testResult.node.source.line,
+      testResult.node.source.column
+    );
+    console.log('\x1b[32m%s\x1b[0m', testResult.inspect());
+    process.exit(1);
+  }
+
+  if (idx > 0) console.log();
+  console.log('\x1b[4mTestcase #%s\x1b[0m', idx + 1);
+
+  const partOneExpected = testEnvironment.getSection('part_one');
+  const partTwoExpected = testEnvironment.getSection('part_two');
+
+  if (partOneExpected.length === 0 && partTwoExpected.length === 0) {
+    console.log('No expectations');
+    return;
+  }
+
+  if (partOneExpected.length > 1) {
+    printSourcePreview(
+      filename,
+      source,
+      partOneExpected[1].name.source.line,
+      partOneExpected[1].name.source.column
+    );
+    console.log(
+      '\x1b[32m%s\x1b[0m',
+      'Runtime error: Expected a single testcase `part_one` section'
+    );
+    process.exit(1);
+  }
+
+  if (partTwoExpected.length > 1) {
+    printSourcePreview(
+      filename,
+      source,
+      partTwoExpected[1].name.source.line,
+      partTwoExpected[1].name.source.column
+    );
+    console.log(
+      '\x1b[32m%s\x1b[0m',
+      'Runtime error: Expected a single testcase `part_two` section'
+    );
+    process.exit(1);
+  }
+
+  const testInput = testEnvironment.getSection('input');
+  if (testInput.length > 1) {
+    printSourcePreview(
+      filename,
+      source,
+      testInput[1].name.source.line,
+      testInput[1].name.source.column
+    );
+    console.log('\x1b[32m%s\x1b[0m', 'Runtime error: Expected a single testcase `input` section');
+    process.exit(1);
+  }
+
+  const testInputResult = evaluateSection(testInput[0], testEnvironment);
+
+  const partOneExpectedResult = evaluateSection(partOneExpected[0], testEnvironment);
+  const partOneActualResult = evaluateSection(partOne[0], testEnvironment, testInputResult);
+
+  if (partOneExpectedResult.equals(partOneActualResult)) {
+    console.log('Part 1: %s \x1b[32m✔️\x1b[0m', partOneActualResult.inspect());
+  } else {
+    console.log(
+      'Part 1: %s \x1b[31m✘ (Expected: %s)\x1b[0m',
+      partOneActualResult.inspect(),
+      partOneExpectedResult.inspect()
+    );
+    exitCode = 1;
+  }
+
+  const partTwoExpectedResult = evaluateSection(partTwoExpected[0], testEnvironment);
+  const partTwoActualResult = evaluateSection(partTwo[0], testEnvironment, testInputResult);
+
+  if (partTwoExpectedResult.equals(partTwoActualResult)) {
+    console.log('Part 2: %s \x1b[32m✔️\x1b[0m', partTwoActualResult.inspect());
+  } else {
+    console.log(
+      'Part 2: %s \x1b[31m✘ (Expected: %s)\x1b[0m',
+      partTwoActualResult.inspect(),
+      partTwoExpectedResult.inspect()
+    );
+    exitCode = 1;
+  }
+});
+
+process.exit(exitCode);
