d0202fd1970bc8786067b2d27fc4655c9488177c
Edd Mann <the@eddmann.com>
2022-11-02 23:44:40 +0000

    Add 2018/08 example

--- src/web/pages/index.tsx
+++ src/web/pages/index.tsx
@@ -180,6 +180,9 @@ const WorkspaceEditor = () => {
             <option value="https://raw.githubusercontent.com/eddmann/advent-of-code/master/2018/santa-lang/aoc2018_day07.santa">
               aoc2018_day07.santa
             </option>
+            <option value="https://raw.githubusercontent.com/eddmann/advent-of-code/master/2018/santa-lang/aoc2018_day08.santa">
+              aoc2018_day08.santa
+            </option>
           </select>
         </div>
         <div>
