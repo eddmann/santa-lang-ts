a7c989a664c58b238d5bbbc7c33f217cc239ce3d
eddmann <the@eddmann.com>
2022-11-15 23:33:11 +0000

    Deploying to gh-pages from @ eddmann/santa-lang-ts@8b62b2c55db0aa23619be30402957d8dae3e0ad8 🚀

--- _next/static/3I7vj6-_FX_e4bPeH9EP1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/742-6093dde2f852f94b.js","static/chunks/pages/index-ec4e52010604bc85.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
